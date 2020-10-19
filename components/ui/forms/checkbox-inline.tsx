e8663442c50d7ddddb97c876da343b81db84057d
Julian Alvarez <julianfullstackdev@gmail.com>
2020-10-19 11:54:49 -0500

    State form in step three completed

--- components/ui/forms/checkbox-inline.tsx
+++ components/ui/forms/checkbox-inline.tsx
@@ -1,16 +1,34 @@
+import { ErrorMessage } from '@hookform/error-message';
+
 type Props = JSX.IntrinsicElements['label'] & {
   label: string;
   name: string;
+  register: any;
+  errors?: {};
+  errorMsg?: string;
+  isRequired?: boolean;
 };
 
 // Here Im using a Tailwind Plugin called tailwindcss-custom-forms, that the form-checkbox className
 // Docs https://tailwindcss-custom-forms.netlify.app/
 
-const CheckboxInline = ({ label, name, ...props }: Props) => {
+const CheckboxInline = ({ label, name, register, errors, errorMsg, isRequired, ...props }: Props) => {
   return (
     <label className="inline-flex items-center mt-2 cursor-pointer" {...props}>
-      <input type="checkbox" name={name} className="form-checkbox h-5 w-5 cursor-pointer" />
+      <input
+        type="checkbox"
+        name={name}
+        className="form-checkbox h-5 w-5 cursor-pointer"
+        ref={register({ required: isRequired ? (errorMsg ? errorMsg : 'This is required') : false })}
+      />
       <span className="ml-3 text-md">{label}</span>
+      {errors && (
+        <ErrorMessage
+          render={({ message }) => <span className="text-right text-sm">{message}</span>}
+          errors={errors}
+          name={name}
+        />
+      )}
     </label>
   );
 };
--- components/worker-compensation-form/step-three.tsx
+++ components/worker-compensation-form/step-three.tsx
@@ -1,40 +1,68 @@
+import { useForm } from 'react-hook-form';
+import { useStateMachine } from 'little-state-machine';
+import useRouter from 'next/router';
+
 import FormWhite from 'components/ui/forms/form-white';
 import InputVertical from 'components/ui/forms/input-vertical';
-
-import useRouter from 'next/router';
 import CheckboxInline from 'components/ui/forms/checkbox-inline';
-const StepThree = () => {
-  const router = useRouter;
 
-  const onSubmit = (e) => {
-    e.preventDefault();
+import updateStore from 'utils/updateStore';
+
+const StepThree = () => {
+  const formId = '12345';
+  const { state, action } = useStateMachine(updateStore);
+
+  const { handleSubmit, errors, register, watch } = useForm({
+    defaultValues: state[formId],
+  });
+
+  const router = useRouter;
+  const watchPaidVacation = watch('hasPaidVacation', false);
+
+  const onSubmit = (data) => {
+    action({ formId, data });
     router.push('/step-four');
   };
 
   return (
     <section className="max-w-full h-auto sm:h-screen  container w-full sm:w-1/2 mx-auto">
-      <FormWhite onSubmit={onSubmit} back="/step-two">
+      <FormWhite onSubmit={handleSubmit(onSubmit)} back="/step-two">
         <h3 className="mb-6 font-semibold text-lg">Tell us about your employees</h3>
 
         <InputVertical
           label="What's the name of the clinic, physician, or ER used for work injuries?"
           inputType="text"
-          name="companyName"
+          name="providerName"
+          register={register}
+          errors={errors}
+          errorMsg="Please add the clinic, phsician or ER name"
+          isRequired
         />
 
         <CheckboxInline
           style={{ marginTop: '20px' }}
           label="Do you provide group medical insurance?"
-          name="isMedicalInsurance"
+          name="hasMedicalInsurance"
+          register={register}
         />
-        <CheckboxInline label="Do you offer a retirement or pension plan?" name="isGroupMedicalInsurance" />
-        <CheckboxInline label="Do you give paid vacation?" name="isPaidVacation" />
+        <CheckboxInline
+          label="Do you offer a retirement or pension plan?"
+          name="hasGroupMedicalInsurance"
+          register={register}
+        />
+        <CheckboxInline label="Do you give paid vacation?" name="hasPaidVacation" register={register} />
 
-        <InputVertical
-          label="Please provide details about the paid vacation"
-          inputType="text"
-          name="detailsPaidVacation"
-        />
+        {watchPaidVacation && (
+          <InputVertical
+            label="Please provide details about the paid vacation"
+            inputType="text"
+            name="detailsPaidVacation"
+            errors={errors}
+            errorMsg="The details are required"
+            register={register}
+            isRequired
+          />
+        )}
       </FormWhite>
     </section>
   );
