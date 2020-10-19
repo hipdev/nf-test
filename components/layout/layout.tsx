b70c7ca782d55a73a8d3baa7068e51206b676cba
Julian Alvarez <julianfullstackdev@gmail.com>
2020-10-19 07:29:23 -0500

    added basic layout, now I will start the first step

---
+++ components/layout/layout.tsx
@@ -0,0 +1,36 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+const Layout = ({ children, title }) => {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/static/favicon.ico" />
+        <title>{title}</title>
+      </Head>
+      <header className="relative shadow-sm">
+        <div className="flex items-center justify-between container mx-auto py-4 px-4">
+          <div className="flex items-center">
+            <img className="w-6 sm:w-8 mr-4" src="/static/favicon.ico" />
+            <h3 className="text-sm sm:text-md font-medium">Worker's compensation application</h3>
+          </div>
+
+          <Link href="#profile">
+            {/* Test link, a future profile of this test hahaha */}
+            <a className="text-xl text-blue-500">
+              <img className="w-10 h-10 rounded-full " src="/static/julian.jpg" />
+            </a>
+          </Link>
+        </div>
+      </header>
+      <main className="bg-gray-100 pt-10 sm:pt-12 xl:pt-20 pb-20 sm:pb-0">
+        <div className="container mx-auto">{children}</div>
+      </main>
+      <footer className="p-3 text-white text-center bg-gray-800 text-sm">
+        <span>© Copyright 2020 Newfront Insurance – License #0L76672</span>
+      </footer>
+    </>
+  );
+};
+
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
+import LayoutForm from 'components/layout/layout';
+
 export default function IndexPage() {
-  return <p>This is my boilerplate with Tailwind :D !</p>;
+  return (
+    <LayoutForm title="Index page - Something nice is cooking">
+      <p>This is my boilerplate with Tailwind :D !</p>
+    </LayoutForm>
+  );
 }
